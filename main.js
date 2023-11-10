d4f6583bf3adbd2a9078ceb5f81fd48b803cd9e3
aluraestudante09 <106967547+aluraestudante09@users.noreply.github.com>
2023-11-10 10:50:39 -0300

    .1

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 function tocaSom(idElementoAudio){
     document.querySelector(idElementoAudio).play();
 }
-const listaDeTeclas= doument.querySelectoeAll(".tecla");
+const listaDeTeclas= document.querySelectoeAll(".tecla");
 
 let contador=0;
 while (coontador<listaDeTeclas.length){
